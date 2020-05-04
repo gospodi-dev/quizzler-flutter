5a193fb2f7febeff4cbec84d8d8574fb9adc29b9
gospodi <gospodijazzet@gmail.com>
2020-05-04 15:57:00 +0300

    добавлена строка счетчика правильны/неверных ответов

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,17 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeepern = [
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +48,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                'Чего за вопросы вытут собираетесь задавать?.',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +73,12 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                scoreKeepern.add(
+                  Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  ),
+                );
               },
             ),
           ),
@@ -84,7 +101,9 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeepern,
+        )
       ],
     );
   }
